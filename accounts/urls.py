9e7f8532b9b26ccc127c9e4c5da3f67ce49cdcf2
Jana <bucujana993@gmail.com>
2025-01-14 03:08:46 +0800

    incorporate the frontend

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import views
 from .views import SignUpView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path("signup/", SignUpView.as_view(), name="signup"),  # Add this line for login
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- dormmatch/views.py
+++ dormmatch/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class WaitlistPageView(TemplateView):
+    template_name = "waitlist.html"
+
+class SolodormPageView(TemplateView):
+    template_name = 'solodorm.html'
+
+class AboutusPageView(TemplateView):
+    template_name = 'aboutus.html'
